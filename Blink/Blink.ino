5cd156274332493716fc6e855d1c0b221329277f
Steve Hyde <steve.hyde@ndcls.ox.ac.uk>
2013-05-08 21:38:38 +0100

    Revert "minor edit"
    This reverts commit 64456a545d44052713314165b29100d9ba03c459.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,6 +21,4 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
-  
-  // A new edit
 }
